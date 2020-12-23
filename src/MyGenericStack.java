a51aed6ab05a93437846e2487e5dfffc15600243
trungbe <trungnd0601@gmail.com>
2020-12-23 10:31:00 +0700

    stack for linkedlist

---
+++ src/MyGenericStack.java
@@ -0,0 +1,33 @@
+import java.util.EmptyStackException;
+import java.util.LinkedList;
+
+public class MyGenericStack<T> {
+    private LinkedList<T> stack;
+
+    public MyGenericStack() {
+        stack = new LinkedList<>();
+    }
+
+    public void push(T e) {
+        stack.addFirst(e);
+    }
+
+    public boolean isEmpty() {
+        if (stack.size() == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return stack.removeFirst();
+    }
+
+    public int size() {
+        return stack.size();
+    }
+
+}
